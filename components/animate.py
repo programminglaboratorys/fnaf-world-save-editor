e251b1f9d9d74aee03a0e6a07cae604b0cec8c86
programminglaboratorys <programming.laboratorys@gmail.com>
2025-04-03 22:58:02 +0300

    feat: new module Components

---
+++ components/animate.py
@@ -0,0 +1,144 @@
+""" module that holds the Animation functionality"""
+from typing import overload
+from dataclasses import dataclass, field
+
+import pygame
+
+from textures import get_surface_hotspot, TVector2
+
+class AnimationNotFound(KeyError):
+    """ Error raised when an animation is not found. """
+
+@dataclass
+class Animation:
+    """ Animation class.
+    """
+    frames: list = field(repr=False)
+    """ The list of frames to cycle through. """
+    speed: int
+    """ The speed at which to cycle through the frames. """
+    repeat: int
+    """ The number of times to repeat the animation. -1 for infinite. """
+    loop: bool = field(default=False, init=False, repr=False)
+    """ Whether the animation should loop. Defaults to False. """
+    current_frame: int = field(default=0, init=False, repr=False)
+    """ The current frame of the animation. """
+    stop: bool = field(default=False, init=False, repr=False)
+    """ Whether the animation should stop. """
+
+    def _post_init_(self):
+        self.loop = self.repeat == -1
+
+    @property
+    def static(self):
+        """ Whether the animation is static."""
+        return self.speed == 0 or len(self.frames) == 1
+
+    def animate(self):
+        """ Go to the next frame """
+        if self.static:
+            return
+        self.current_frame = (self.current_frame + 1) % len(self.frames)
+
+    def set_current_frame(self, frame: int):
+        """ Set the current frame """
+        self.current_frame = frame % len(self.frames)
+
+    def draw(self, window, position):
+        """ Draw frame(s) """
+        hotspot = get_surface_hotspot(self.frames[self.current_frame]) or TVector2((0, 0))
+        window.blit(self.frames[self.current_frame], (position[0] - hotspot.x, position[1] - hotspot.y))
+
+class AnimatatedObject:
+    """ An object that can be animated. """
+    _current_animation: str = ""
+
+    animations: dict[str, Animation]
+    stop: bool = False
+
+    def __init__(self, animations: dict[str, Animation] = None):
+        self.animations = {} if animations is None else animations
+        self.elapsed = 0
+        if list(self.animations):
+            self.change_animation(0)
+
+    @overload
+    def change_animation(self, index: int, *, reset: bool = True):
+        """ Change the animation by index. """
+
+    @overload
+    def change_animation(self, name: str, *, reset: bool = True):
+        """ Change the animation by name. """
+
+    def change_animation(self, name: int | str, *, reset: bool = True):
+        """
+        Change the animation.
+        """
+        if isinstance(name, int):
+            name = list(self.animations)[max(min(name, len(self.animations)-1), 0)]
+        self._current_animation = name
+        if reset:
+            self.current_animation.set_current_frame(0)
+
+    @property
+    def empty(self) -> bool:
+        """ Check if the object is empty. """
+        return len(self.animations) == 0
+
+    @property
+    def current_animation(self) -> Animation:
+        """ Get the current animation. """
+        try:
+            return self.animations[self._current_animation]
+        except KeyError:
+            if self._current_animation == "":
+                raise AnimationNotFound("No animation selected") from None
+            raise AnimationNotFound(f"Animation '{self._current_animation}' not found") from None
+
+    def add_animation(self, name: str, animation: Animation):
+        """ Add/Override an animation.
+        if the animation already exists, it will be overridden
+        """
+        self.animations[name] = animation
+
+    def draw(self, window, clock: pygame.time.Clock, position):
+        """ Draw the current animation. """
+        animation = self.current_animation
+        self.elapsed += clock.get_time()
+        if self.elapsed > animation.speed:
+            #self.elapsed -= animation.speed
+            self.elapsed = 0
+            animation.animate()
+        animation.draw(window, position)
+
+if __name__ == "__main__":
+    pygame.init()
+    twindow = pygame.display.set_mode((500, 500))
+    freddy = AnimatatedObject()
+    frames = [pygame.image.load(f"textures/characters/freddy/freddy frame  ({i}).png") for i in range(1, 11+1)]
+    freddy.add_animation("freddy", Animation(frames, speed=40, repeat=-1))
+    freddy.change_animation("freddy")
+    tclock = pygame.time.Clock()
+    font = pygame.font.SysFont("Arial", 30)
+    FPS = 60
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    FPS += 10
+                elif event.key == pygame.K_DOWN:
+                    FPS -= 10
+                if event.key == pygame.K_RIGHT:
+                    FPS = 60
+                elif event.key == pygame.K_LEFT:
+                    FPS = 120
+
+        twindow.fill((0, 0, 0))
+        fps_text = font.render(f"PFPS: {FPS}. FPS: {round(tclock.get_fps())}", True, (255, 255, 255))
+        twindow.blit(fps_text, (10, 10))
+        freddy.draw(twindow, tclock, (250, 250))
+        pygame.display.flip()
+        tclock.tick(FPS)
---
+++ components/textbox.py
@@ -0,0 +1,159 @@
+"""TextBox component for Pygame.
+
+This module provides a `TextBox` class which you can use to create
+text boxes in your Pygame application. The text box handles keyboard
+input and can be used to input text from the user. The text box can
+be customized in various ways, such as setting the color, border
+thickness, and font. The text box can also be set to be inactive
+when the user presses enter.
+
+originally created by Mekire, improved version by iamme
+
+"""
+import re
+import pygame as pg
+from typing import Callable
+
+
+ACCEPTED = r"[a-zA-Z0-9.\"#$%&'()*+,-./:;<=>?@\[\\\]\^_`{\|}~]*"
+
+class TextBox:
+    """ TextBox component for Pygame. """
+    #rect: pg.Rect
+    buffer: list
+    final: str
+    rendered: pg.Surface
+    render_rect: pg.Rect
+    render_area: pg.Rect
+    blink: bool
+    blink_timer: float
+    transparent: bool
+    id: str
+    command: Callable[[str, str], None]
+    active: bool
+    color: pg.Color
+    font_color: pg.Color
+    outline_color: pg.Color
+    invalid_color: pg.Color
+    outline_width: int
+    active_color: pg.Color
+    font: pg.font.Font
+    clear_on_enter: bool
+    inactive_on_enter: bool
+    outline_thickness: int
+    broder_redius: int
+    regex: re.Pattern
+    invalid: bool
+
+    def __init__(self, rect, buffer=None, blink=True, blink_timer=0.0, transparent=False, 
+                 id=None, command=None, active=False, color=pg.Color("white"), font_color=pg.Color("black"), 
+                 outline_color=pg.Color("black"), invalid_color=pg.Color("red"), outline_width=2,
+                 active_color=pg.Color("blue"), font=None, clear_on_enter=False, inactive_on_enter=True,
+                 outline_thickness=2, broder_redius=3, regex=re.compile(ACCEPTED)):
+        self.rect = pg.Rect(rect)
+        self.buffer = buffer if buffer is not None else []
+        self.blink = blink
+        self.blink_timer = blink_timer
+        self.transparent = transparent
+        self.id = id
+        self.command = command
+        self.active = active
+        self.color = color
+        self.font_color = font_color
+        self.outline_color = outline_color
+        self.invalid_color = invalid_color
+        self.outline_width = outline_width
+        self.active_color = active_color
+        self.font = font
+        self.clear_on_enter = clear_on_enter
+        self.inactive_on_enter = inactive_on_enter
+        self.outline_thickness = outline_thickness
+        self.broder_redius = broder_redius
+        self.regex = regex
+        #
+        self.invalid = False
+        self.final = None
+        self.rendered = None
+        self.render_rect = None
+        self.render_area = None
+
+    def vaildate(self, char: str):
+        """ check if char is valid according to regex """
+        return self.regex.match(char) is not None
+
+    def get_event(self,event):
+        if event.type == pg.KEYDOWN and self.active:
+            if event.key in (pg.K_RETURN,pg.K_KP_ENTER):
+                self.execute()
+            elif event.key == pg.K_BACKSPACE:
+                if self.buffer:
+                    self.buffer.pop()
+            elif event.unicode:
+                if self.vaildate(event.unicode):
+                    self.invalid = False
+                    self.buffer.append(event.unicode)
+                else:
+                    self.invalid = True
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            self.active = self.rect.collidepoint(event.pos)
+            print("TEST", self.rect.collidepoint(event.pos), "FOR", self.id, "RENDER RECT", self.rect, "RERE INSTEAD", self.render_rect.collidepoint(event.pos))
+
+    def execute(self):
+        if self.command:
+            self.command(self.id, self.final)
+        self.active = not self.inactive_on_enter
+        if self.clear_on_enter:
+            self.buffer = []
+
+    def render_font(self, text) -> pg.Surface:
+        """ render text in font and color """
+        return self.font.render(text, True, self.font_color)
+    
+    def force_update(self):
+        self.final = "".join(self.buffer)
+        self.rendered = self.render_font(self.final)
+        self.render_rect = self.rendered.get_rect(x=self.rect.x+2,
+                                                  centery=self.rect.centery)
+        if self.render_rect.width > self.rect.width-6:
+            offset = self.render_rect.width-(self.rect.width-6)
+            self.render_area = pg.Rect(offset,0,self.rect.width-6,
+                                       self.render_rect.height)
+        else:
+            self.render_area = self.rendered.get_rect(topleft=(0,0))
+
+    def update(self):
+        if not self.active and self.final is not None: # an update is not necessary
+            return
+        new = "".join(self.buffer)
+        if new != self.final:
+            self.final = new
+            self.rendered = self.render_font(self.final)
+            self.render_rect = self.rendered.get_rect(x=self.rect.x+2,
+                                                      centery=self.rect.centery)
+            if self.render_rect.width > self.rect.width-6:
+                offset = self.render_rect.width-(self.rect.width-6)
+                self.render_area = pg.Rect(offset,0,self.rect.width-6,
+                                           self.render_rect.height)
+            else:
+                self.render_area = self.rendered.get_rect(topleft=(0,0))
+        if pg.time.get_ticks()-self.blink_timer > 200:
+            self.blink = not self.blink
+            self.blink_timer = pg.time.get_ticks()
+
+    def draw(self, surface):
+        outline_color = self.active_color if self.active else self.outline_color
+        if self.invalid:
+            outline_color = self.invalid_color
+        outline = self.rect.inflate(self.outline_width*2,self.outline_width*2)
+        if not self.transparent:
+            if outline_color: 
+                pg.draw.rect(surface, outline_color, outline,self. outline_thickness, self.broder_redius)
+            if self.color: 
+                surface.fill(self.color, self.rect)
+        if self.rendered:
+            surface.blit(self.rendered, self.render_rect, self.render_area)
+        if self.blink and self.active:
+            curse = self.render_area.copy()
+            curse.topleft = self.render_rect.topleft
+            surface.fill(self.font_color, (curse.right+1,curse.y,2,curse.h))
+
