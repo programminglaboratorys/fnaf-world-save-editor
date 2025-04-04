4533f55d9a887b1bff905c85c3f90cc60c774054
programminglaboratorys <programming.laboratorys@gmail.com>
2025-04-04 07:40:29 +0300

    chore: removed some functionality to modules from the "textures" module

--- textures.py
+++ textures.py
@@ -1,23 +1,8 @@
 """ Textures and helper functions for textures """
-from typing import Optional, Union
-from functools import lru_cache
-from helper import subtract_vectors
 import pygame
-import glob
-class TVector2(tuple):
-    """ 2D vector """
-    @property
-    def x(self):
-        """ x component of the vector """
-        return self[0]
+from typing import Optional, Union
 
-    @property
-    def y(self):
-        """ y component of the vector """
-        return self[1]
-
-    def __repr__(self):
-        return f"TVector2(x={self.x}, y={self.y})"
+from geometry import TVector2
 
 _textures_hotspot_table: dict[pygame.Surface, TVector2] = {}
 
@@ -52,59 +37,3 @@ def load_image(path: str, hotspot: Optional[Union[tuple[int, int], str]] = None)
 def get_surface_hotspot(surface: pygame.Surface) -> Optional[TVector2]:
     """ get the hotspot of a surface """
     return _textures_hotspot_table.get(surface)
-
-def draw_background(window, image):
-    """
-    Draws a background image with a hotspot.
-    The image is placed such that the position is at (0, 0) in the window.
-    """
-    hotspot = get_surface_hotspot(image)
-    window.blit(image, (-hotspot.x, -hotspot.y))
-
-class Textures: # create a texture manager? is it worth it?
-    """ holds the textures used in the editor """
-
-    button = load_image('textures/save-button.png', hotspot="center")
-    button_selected = load_image('textures/save-button-selected.png', hotspot="center")
-    freddy = load_image('textures/characters/freddy.png', hotspot=(125, 220))
-    background = load_image('textures/background.png')
-
-
-@lru_cache
-def _circlepoints(r):
-    x, y, e = r, 0, 1 - r
-    points = []
-    while x >= y:
-        points.append((x, y))
-        y += 1
-        if e < 0:
-            e += 2 * y - 1
-        else:
-            x -= 1
-            e += 2 * (y - x) - 1
-    points += [(y, x) for x, y in points if x > y]
-    points += [(-x, y) for x, y in points if x]
-    points += [(x, -y) for x, y in points if y]
-    points.sort()
-    return points
-
-def render_text_with_outline(text: str, font: pygame.Font, gfcolor=pygame.Color('dodgerblue'), ocolor=(0, 0, 0), opx=2):
-    """
-    Render text with an outline
-    """
-    textsurface = font.render(text, True, gfcolor).convert_alpha()
-    w = textsurface.get_width() + 2 * opx
-    h = font.get_height()
-
-    osurf = pygame.Surface((w, h + 2 * opx)).convert_alpha()
-    osurf.fill((0, 0, 0, 0))
-
-    surf = osurf.copy()
-
-    osurf.blit(font.render(text, True, ocolor).convert_alpha(), (0, 0))
-
-    for dx, dy in _circlepoints(int(round(opx))):
-        surf.blit(osurf, (dx + opx, dy + opx))
-
-    surf.blit(textsurface, (opx, opx))
-    return surf
