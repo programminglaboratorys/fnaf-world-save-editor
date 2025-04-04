cafc69cfe7a195fd80c79a7c19ce3c8de474dbbe
programminglaboratorys <programming.laboratorys@gmail.com>
2025-04-04 07:40:52 +0300

    feat: new module "resources "that includes FontBank, Textures, LazyAttributes

---
+++ resources.py
@@ -0,0 +1,40 @@
+""" a module for resources like fonts and images """
+# pylint: disable=too-few-public-methods
+import pygame
+from helper import instantiate
+from textures import load_image
+
+CHARACTER_TEXTURES_PATH = "textures/characters/"
+
+class LazyAttributes:
+    """
+    A class that loads attributes lazily when they are accessed
+    """
+    _loaded_attributes = {}
+    def __getattribute__(self, name):
+        if name.startswith("_"):
+            return super().__getattribute__(name)
+        attr = super().__getattribute__(name)
+        if callable(attr):
+            # print("attribute", name, "value", attr, flush=True)
+            if name not in self._loaded_attributes:
+                self._loaded_attributes[name] = attr()
+            return self._loaded_attributes[name]
+        return attr
+
+@instantiate()
+class FontBank(LazyAttributes):
+    """ A class that loads fonts lazily when they are accessed """
+    arialnb_font: pygame.font.Font = lambda self: pygame.font.Font("textures/fonts/ARIALNB.TTF", 30)
+    lcd_font: pygame.font.Font = lambda self: pygame.font.Font("textures/fonts/LcdSolid.ttf", 30)
+
+
+class Textures: # create a texture manager? is it worth it?
+    """ holds the textures used in the editor """
+
+    button = load_image('textures/save-button.png', hotspot="center")
+    button_selected = load_image('textures/save-button-selected.png', hotspot="center")
+    freddy = load_image('textures/characters/freddy.png', hotspot=(125, 220))
+    background = load_image('textures/background.png')
+
+
