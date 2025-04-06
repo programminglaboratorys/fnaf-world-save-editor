a7b9f4f7748d913f8c2bbcc0233358773c2205af
programminglaboratorys <programming.laboratorys@gmail.com>
2025-04-06 03:11:22 +0300

    fix: pylint errors and added todos

--- components/textbox.py
+++ components/textbox.py
@@ -11,14 +11,15 @@ originally created by Mekire, improved version by iamme
 
 """
 import re
-import pygame as pg
 from typing import Callable
 
+import pygame as pg
 
 ACCEPTED = r"[a-zA-Z0-9.\"#$%&'()*+,-./:;<=>?@\[\\\]\^_`{\|}~]*"
-
+# TODO: give better comments and change it to a dataclass, solve sensible pylint warnings
+# pylint: disable=too-many-instance-attributes
 class TextBox:
-    """ TextBox component for Pygame. """
+    """ TextBox component. """
     #rect: pg.Rect
     buffer: list
     final: str
@@ -45,15 +46,16 @@ class TextBox:
     regex: re.Pattern
     invalid: bool
 
-    def __init__(self, rect, buffer=None, blink=True, blink_timer=0.0, transparent=False, 
-                 id=None, command=None, active=False, color=pg.Color("white"), font_color=pg.Color("black"), 
+    # pylint: disable=too-many-arguments, too-many-positional-arguments, too-many-locals,
+    def __init__(self, rect, buffer=None, blink=True, transparent=False,
+                 id=None, command=None, active=False, color=pg.Color("white"), font_color=pg.Color("black"),
                  outline_color=pg.Color("black"), invalid_color=pg.Color("red"), outline_width=2,
                  active_color=pg.Color("blue"), font=None, clear_on_enter=False, inactive_on_enter=True,
                  outline_thickness=2, broder_redius=3, regex=re.compile(ACCEPTED)):
         self.rect = pg.Rect(rect)
         self.buffer = buffer if buffer is not None else []
         self.blink = blink
-        self.blink_timer = blink_timer
+        self.blink_timer = 0
         self.transparent = transparent
         self.id = id
         self.command = command
@@ -82,6 +84,7 @@ class TextBox:
         return self.regex.match(char) is not None
 
     def get_event(self,event):
+        """ get event and process it """
         if event.type == pg.KEYDOWN and self.active:
             if event.key in (pg.K_RETURN,pg.K_KP_ENTER):
                 self.execute()
@@ -96,7 +99,11 @@ class TextBox:
                     self.invalid = True
         elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             self.active = self.rect.collidepoint(event.pos)
-            print("TEST", self.rect.collidepoint(event.pos), "FOR", self.id, "RENDER RECT", self.rect, "RERE INSTEAD", self.render_rect.collidepoint(event.pos))
+            # TODO: remove
+            #print(repr(self.id),
+            # "RECT CLLIDE", self.rect.collidepoint(event.pos),
+            # "RECT", self.rect,
+            # "RENDER RECT CLLIDE", self.render_rect.collidepoint(event.pos))
 
     def execute(self):
         """ execute the command with the current self.final as argument """
@@ -109,7 +116,7 @@ class TextBox:
     def render_font(self, text) -> pg.Surface:
         """ render text in font and color """
         return self.font.render(text, True, self.font_color)
-    
+
     def force_update(self):
         """ force an update of the text box without needing to be focused """
         self.final = "".join(self.buffer)
@@ -122,10 +129,19 @@ class TextBox:
                                        self.render_rect.height)
         else:
             self.render_area = self.rendered.get_rect(topleft=(0,0))
+        # TODO: change to use dalte time (check animate.py)
+        if pg.time.get_ticks()-self.blink_timer > 200:
+            self.blink = not self.blink
+            self.blink_timer = pg.time.get_ticks()
 
     def update(self):
+        """ update the blink timer """
         if not self.active and self.final is not None: # an update is not necessary
             return
+        # TODO: change to use dalte time (check animate.py)
+        if pg.time.get_ticks()-self.blink_timer > 200:
+            self.blink = not self.blink
+            self.blink_timer = pg.time.get_ticks()
         new = "".join(self.buffer)
         if new != self.final:
             self.final = new
@@ -138,24 +154,21 @@ class TextBox:
                                            self.render_rect.height)
             else:
                 self.render_area = self.rendered.get_rect(topleft=(0,0))
-        if pg.time.get_ticks()-self.blink_timer > 200:
-            self.blink = not self.blink
-            self.blink_timer = pg.time.get_ticks()
 
     def draw(self, surface):
+        """ draw the text box """
         outline_color = self.active_color if self.active else self.outline_color
         if self.invalid:
             outline_color = self.invalid_color
         outline = self.rect.inflate(self.outline_width*2,self.outline_width*2)
         if not self.transparent:
-            if outline_color: 
+            if outline_color:
                 pg.draw.rect(surface, outline_color, outline,self. outline_thickness, self.broder_redius)
-            if self.color: 
+            if self.color:
                 surface.fill(self.color, self.rect)
         if self.rendered:
             surface.blit(self.rendered, self.render_rect, self.render_area)
         if self.blink and self.active:
             curse = self.render_area.copy()
             curse.topleft = self.render_rect.topleft
-            surface.fill(self.font_color, (curse.right+1,curse.y,2,curse.h))
-
+            surface.fill(self.font_color, (curse.right+1, curse.y, 2, curse.h))
