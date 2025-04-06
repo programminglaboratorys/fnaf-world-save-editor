da05c912aa2fb1b58a4a9eb1df5c8367b8b4ff61
programminglaboratorys <programming.laboratorys@gmail.com>
2025-04-06 04:55:11 +0300

    chore: refacter and simplified global_event_handler code

--- constants.py
+++ constants.py
@@ -19,16 +19,14 @@ def global_event_handler(state: State, event):
     """
     global event handler, handles pygame.QUIT and pygame.VIDEORESIZE
     """
-    if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+    if event.type == pygame.WINDOWFOCUSLOST:
+        state.focused = False
+    elif event.type == pygame.WINDOWFOCUSGAINED:
+        state.focused = True
+    elif event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_q:
         raise ExitGame()
-    if event.type == pygame.VIDEORESIZE:
+    elif event.type == pygame.VIDEORESIZE:
         width, height = event.size
-        if width < MIN_WINDOW_SIZE[0]:
-            width = MIN_WINDOW_SIZE[0]
-        if width > MAX_WINDOW_SIZE[0]:
-            width = MAX_WINDOW_SIZE[0]
-        if height < MIN_WINDOW_SIZE[1]:
-            height = MIN_WINDOW_SIZE[1]
-        if height > MAX_WINDOW_SIZE[1]:
-            height = MAX_WINDOW_SIZE[1]
+        width = min(max(width, MIN_WINDOW_SIZE[0]), MAX_WINDOW_SIZE[0])
+        height = min(max(height, MIN_WINDOW_SIZE[1]), MAX_WINDOW_SIZE[1])
         state.window = pygame.display.set_mode((width, height), pygame.RESIZABLE)
